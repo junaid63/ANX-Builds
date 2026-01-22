9057c491b870770ff7e19576ecc448a261ecda40
mohsinkhan89 <mk8979494@gmail.com>
2026-01-22 13:23:58 +0500

    changes done technical and some contact page

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -11,7 +11,6 @@
 
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1">
         <meta property="og:url" content="{{ url()->current() }}">
